e3c4bf36100eed8de91fd3279ab82a89a9bff3f0
RodrigoBurgarelli <rodrigoburgarelli@gmail.com>
2025-02-21 01:33:44 -0300

    atualização na parte superior da main da tela de solicitação

--- src/Components/solicitacao/Solicitacao.jsx
+++ src/Components/solicitacao/Solicitacao.jsx
@@ -49,12 +49,13 @@ export default function Solicitacao() {
                         <div className={styles.barraVertical}></div>
 
                         <div className={styles.grupo2}>
-                            <div className={styles.inputDate}>
+                            <div className={styles.inputData}>
                                 <label htmlFor="">Data</label>
                                 <input type="date" name="" id="" />
 
                             </div>
-                            <div className={styles.inputDespesas}>
+
+                            <div className={styles.inputTipoDespesas}>
                                 <label htmlFor="">Tipo de Despesa</label>
 
                                 <select name="" id="">
@@ -62,10 +63,72 @@ export default function Solicitacao() {
                                     <option value="">Alimentação</option>
                                     <option value="">Combustível</option>
                                     <option value="">Condução</option>
+                                    <option value="">Estacionamento</option>
+                                    <option value="">Viagem admin.</option>
+                                    <option value="">Viagem operacional</option>
+                                    <option value="">Eventos de representação</option>
                                 </select>
-
                             </div>
 
+                            <div className={styles.inputCentroCustos}>
+                                <label htmlFor="">Centro de Custos</label>
+                                <select name="" id="">
+                                   <option value="">Selecionar</option>
+                                   <option value="">1100109002 - FIN CONTROLES INTERNOS MTZ</option>
+                                    <option value="">1100110002 - FIN VICE-PRESIDENCIA FINANCAS MTZ</option>
+                                    <option value="">1100110102 - FIN CONTABILIDADE MTZ</option>
+                                </select>
+                            </div>
+
+                            <div className={styles.inputOrdInt}>
+                                <label htmlFor="">Ord. Int.</label>
+                                <input type="text" name="" id="" />
+                            </div>
+
+                            <div className={styles.inputDiv}>
+                                <label htmlFor="">Div.</label>
+                                <input type="text" name="" id="" />
+                            </div>
+
+                            <div className={styles.inputPep}>
+                                <label htmlFor="">PEP</label>
+                                <input type="text" name="" id="" />
+                            </div>
+
+                            <div className={styles.inputMoeda}>
+                                <label htmlFor="">Moeda</label>
+                                <input type="text" />
+                            </div>
+
+                            <div className={styles.inputDist}>
+                                <label htmlFor="">Dist / Km</label>
+                                <input type="text" name="" id="" />
+                            </div>
+
+                            <div className={styles.inputValor}>
+                                <label htmlFor="">Valor / Km</label>
+                                <input type="text" name="" id="" />
+                            </div>
+
+                            <div className={styles.inputValFaturado}>
+                                <label htmlFor="">Val. Faturado</label>
+                                <input type="text" name="" id="" />
+                            </div>
+
+                            <div className={styles.inputDespesa}>
+                                <label htmlFor="">Despesa</label>
+                                <input type="text" name="" id="" />
+                            </div>
+
+                            <div className={styles.buttonG2}>
+                                <button className={styles.buttonSalvar}>
+                                    <img src="" alt="" />
+                                    <p>Salvar</p>
+                                </button>
+                                <button className={styles.buttonApagar}>
+                                    <img src="" alt="" />
+                                </button>
+                            </div>
                         </div>
 
                     </form>
